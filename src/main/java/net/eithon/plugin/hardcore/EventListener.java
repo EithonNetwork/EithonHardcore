8dd925579dbf8a799cede85970e1beacfd52da4c
vanbrayne <lars.lindberg@xlent.se>
2016-06-30 22:08:32 +0200

    Compiler warnings

--- src/main/java/net/eithon/plugin/hardcore/EventListener.java
+++ src/main/java/net/eithon/plugin/hardcore/EventListener.java
@@ -1,32 +1,22 @@
 package net.eithon.plugin.hardcore;
 
 import net.eithon.library.extensions.EithonPlugin;
-import net.eithon.library.plugin.Logger.DebugPrintLevel;
 
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public final class EventListener implements Listener {
-	private EithonPlugin _eithonPlugin;
 	private Controller _controller;
 
 	public EventListener(EithonPlugin eithonPlugin, Controller controller) {
 		this._controller = controller;
-		this._eithonPlugin = eithonPlugin;
 	}
 
 	@EventHandler
 	public void onPlayerDeathEvent(PlayerDeathEvent event) {
-		debug("onPlayerDeathEvent", "Enter");
 		Player player = event.getEntity();
-		debug("onPlayerDeathEvent", "Ban this player. Return.");
 		this._controller.playerDied(player);
-	}
-
-	private void debug(String method, String message) {
-		this._eithonPlugin.getEithonLogger().debug(DebugPrintLevel.VERBOSE, "%s: %s", method, message);
 	}
 }
